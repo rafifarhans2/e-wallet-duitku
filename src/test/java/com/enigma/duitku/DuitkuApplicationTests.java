14a980a09aefd55cc6685ed4c8b12bb67cf4a4c9
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-11-06 10:53:02 +0700

    Initial Commit

---
+++ src/test/java/com/enigma/duitku/DuitkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.duitku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuitkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
