207e9efe20d5b17fa84106a03944759f500036d5
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-11-07 08:35:43 +0700

    [FEATURE] bank account Controller

---
+++ src/main/java/com/enigma/duitku/controller/BankAccountController.java
@@ -0,0 +1,36 @@
+package com.enigma.duitku.controller;
+
+import com.enigma.duitku.exception.UserException;
+import com.enigma.duitku.model.request.BankAccountRequest;
+import com.enigma.duitku.model.response.BankAccountResponse;
+import com.enigma.duitku.model.response.CommonResponse;
+import com.enigma.duitku.service.BankAccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/bankaccount")
+public class BankAccountController {
+
+    private final BankAccountService bankAccountService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addBankAccount(@RequestBody BankAccountRequest request) throws UserException {
+
+        BankAccountResponse bankAccountResponse = bankAccountService.addAccount(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(CommonResponse.builder()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .data(bankAccountResponse)
+                        .message("Successfully create bank account")
+                        .build());
+    }
+
+}
