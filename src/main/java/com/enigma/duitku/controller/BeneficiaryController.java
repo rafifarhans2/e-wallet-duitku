ab7f3fca495e0728dfbbd0d35f9678647b8623e5
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-11-10 11:43:01 +0700

    [IMPROVE] change RequestMapping path additional /api

--- src/main/java/com/enigma/duitku/controller/BeneficiaryController.java
+++ src/main/java/com/enigma/duitku/controller/BeneficiaryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/beneficiary")
+@RequestMapping("api/beneficiary")
 public class BeneficiaryController {
 
     private final BeneficiaryService beneficiaryService;
